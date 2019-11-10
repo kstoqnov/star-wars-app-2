7ec15a6c41483ffd3efe31c2887ddb6e7d032c2c
kstoqnov <kstoqnov@gmail.com>
2019-11-10 22:01:23 +0200

    add characters pages

---
+++ src/pages/characters/container_components/CharacterDetailsContainerComponent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import CharacterDetailsComponent from '../style_components/CharacterDetailsComponent';
+import SpinnerComponent from '../../app_custom_components/style_components/SpinnerComponent';
+
+import { loader } from 'graphql.macro';
+import { useQuery } from '@apollo/react-hooks';
+ 
+
+const GET_PERSON = loader('../../app_graphql_schemas/getPerson.gql');
+
+
+const CharacterDetailsContainerComponent = ({ match }) => {
+
+  const { data, loading, error } = useQuery(GET_PERSON, {
+    variables: { id: match.params.characterId },
+  });
+
+  if (loading) return <SpinnerComponent/>;
+  if (error) return `Error! ${error}`;
+
+  return <CharacterDetailsComponent person={data.person} />;
+  
+};
+
+export default CharacterDetailsContainerComponent;
+
---
+++ src/pages/characters/container_components/CharactersPageContainerComponent.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import CharactersPageComponent from '../style_components/CharactersPageComponent';
+import SpinnerComponent from '../../app_custom_components/style_components/SpinnerComponent';
+
+import { loader } from 'graphql.macro';
+import { TWELVE_CHARACTERS } from '../../../utils/js/constants';
+import { useQuery } from '@apollo/react-hooks';
+ 
+
+
+const GET_ALL_PEOPLE = loader('../../app_graphql_schemas/getAllPeople.gql');
+
+const CharactersPageContainerComponent = () => {
+
+  const { data, loading, error, fetchMore } = useQuery(GET_ALL_PEOPLE, {
+    variables: { first: TWELVE_CHARACTERS },
+  });
+
+  if (loading) return <SpinnerComponent/>;
+  if (error) return `Error! ${error}`;
+
+  const allPeople = data.allPeople;
+
+  return (
+    <CharactersPageComponent
+      people={allPeople}
+      onLoadMore={() =>
+        fetchMore({
+          variables: {
+            cursor: allPeople.pageInfo.endCursor,
+          },
+          updateQuery: (previousResult, { fetchMoreResult }) => {
+            const newEdges = fetchMoreResult.allPeople.edges;
+            const pageInfo = fetchMoreResult.allPeople.pageInfo;
+
+            return newEdges.length
+              ? {
+                  allPeople: {
+                    __typename: previousResult.allPeople.__typename,
+                    edges: [...previousResult.allPeople.edges, ...newEdges],
+                    pageInfo,
+                  },
+                }
+              : previousResult;
+          },
+        })
+      }
+    />
+  );
+};
+
+export default CharactersPageContainerComponent;
