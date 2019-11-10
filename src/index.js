ca8afdd833e0d020c67d8ba9503d85f81a8f79e0
kstoqnov <kstoqnov@gmail.com>
2019-11-10 23:42:51 +0200

    add type definitions for user and theme

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { setContext } from 'apollo-link-context';
 import { THEME } from './utils/js/constants';
 import { defaultOptions } from './utils/js/defaultOptionsApolloClient';
+import { typeDefs } from './type_definition/typeDefs';
 
 const httpLink = createHttpLink({
     uri: process.env.REACT_APP_API_URL
@@ -32,6 +33,7 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
     link: authLink.concat(httpLink),
     cache,
+    typeDefs,
     name: 'react-web-client',
     version: '1.3',
     queryDeduplication: false,
