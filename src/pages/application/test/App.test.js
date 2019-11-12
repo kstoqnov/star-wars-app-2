7ed01432fff78747902c728be23c208d3ed6fbc7
kstoqnov <kirilstoqnov@gmail.com>
2019-11-12 15:30:35 +0200

    babel-plugin-transform-export-extensions

--- src/pages/application/test/App.test.js
+++ src/pages/application/test/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<>RenderWithThemeForTest(App)</>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// test('Link changes the class when hovered', () => {
+//   const component = renderer.create(
+//     App,
+//   );
+  
+//   expect(component).toMatchSnapshot();
+
+// });
